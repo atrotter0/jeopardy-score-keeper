3fb868c083a35350fc72384c7af59dba481cb122
Abel Trotter <atrotter@entrepreneur.com>
2023-08-31 17:59:12 -0400

    Updates

---
+++ precache-manifest.e9c947569b4976c69ee088f98c9f1e76.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60d0c9ba1863b30ba9484acfdfdf142b",
+    "url": "/jeopardy-score-keeper/index.html"
+  },
+  {
+    "revision": "fa6fdbcf804b306e71af",
+    "url": "/jeopardy-score-keeper/static/js/2.3a0dd3a8.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/jeopardy-score-keeper/static/js/2.3a0dd3a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9446087a311805f0c12d",
+    "url": "/jeopardy-score-keeper/static/js/main.7e00b3e1.chunk.js"
+  },
+  {
+    "revision": "082a58890b2ba4fa3290",
+    "url": "/jeopardy-score-keeper/static/js/runtime-main.12f33800.js"
+  }
+]);
